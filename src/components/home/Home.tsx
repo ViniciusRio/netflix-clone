11b54ae742be7daed1b7a364a7187279879543d4
Vinicius Rio <vlmdr@outlook.com>
2021-06-08 17:41:23 -0300

    Header and Nav preview

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,17 @@
+import './Home.css'
+import Nav from '../nav/Nav';
+import Header from '../header/Header';
+
+function Home() {
+    return (
+        <>
+            <div className="app">
+                <Nav />
+                <Header />
+
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,20 @@
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img
+            className="nav-logo"
+            src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+            alt="logo-netflix" 
+            />
+            <img
+            className="nav-avatar"
+            src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+            alt=""
+            />
+      </div>
+    );
+}
+
+export default Nav;
