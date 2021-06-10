2769ea1af23bec5c72a4720d1436cddc7c97d3a7
Vinicius Rio <vlmdr@outlook.com>
2021-06-10 10:29:51 -0300

    rows, movies and env

---
+++ src/components/row/Row.tsx
@@ -0,0 +1,12 @@
+function Row() {
+    return (
+        <div className="row">
+            <h2 className="title"></h2>
+            <div className="row-content">
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default Row;
