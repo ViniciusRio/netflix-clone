9093521426c25ddaec5aea8b572ce34ab261f94d
Vinicius Rio <vlmdr@outlook.com>
2021-06-10 17:29:27 -0300

    fetch movies and show

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import './Home.css'
 import Nav from '../nav/Nav';
 import Header from '../header/Header';
+import Row from '../row/Row';
 
 function Home() {
     return (
@@ -8,6 +9,8 @@ function Home() {
             <div className="app">
                 <Nav />
                 <Header />
+                <Row title='Netflix Originals' category='netflix-originals' />
+
 
             </div>
         </>
--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -1,9 +1,36 @@
-function Row() {
+import firebaseDatabase from "../../firebase/firebase";
+import { useEffect, useState } from "react";
+import Movie from "../../interfaces/Movie";
+import MovieParams from "../../interfaces/Row/MovieParams";
+
+function Row(props: MovieParams) {
+    const [movies, setMovies] = useState([]);
+    const {title, category} = props
+
+    const fetchMovies = ((movieType: string) => {
+        const movieRef = firebaseDatabase.ref(`movies/${movieType}`);
+        movieRef.on('value', (snapshot) => {
+            const movies = snapshot.val();            
+            if (movies) {
+                setMovies(() => movies);
+            }
+        });
+    });
+
+    useEffect(() => {
+        fetchMovies(category);
+    }, []);
+
     return (
         <div className="row">
-            <h2 className="title"></h2>
+            <h2 className="title">{title}</h2>
             <div className="row-content">
-                <img src="" alt="" />
+                {movies.map((movie: Movie) => (
+                    <img 
+                    key={movie.id}
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} 
+                    alt={movie.original_name} />
+                ))}
             </div>
         </div>
     );
