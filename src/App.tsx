d40d67fa4afbc08d20c48239984f3611bfd43547
Vinicius Rio <vlmdr@outlook.com>
2021-06-11 17:42:26 -0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Home from './components/home/Home';
+import Login from './components/login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+    );
 }
 
 export default App;
