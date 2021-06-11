5438bf01f15755f5412f4b699e4b601380f11a44
Vinicius Rio <vlmdr@outlook.com>
2021-06-11 17:43:48 -0300

    add more rows

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -11,6 +11,11 @@ function Home() {
                 <Header />
                 <Row title='Netflix Originals' category='netflix-originals' />
                 <Row title='Trending Now' category='trending-now' />
+                <Row title='Top Rated' category='top-rated' />
+                <Row title='Action Movies' category='action-movies' />
+                <Row title='Horror Movies' category='horror-movies' />
+                <Row title='Romance Movies' category='romance-movies' />
+                <Row title='Documentaries Movies' category='documentaries-movies' />
             </div>
         </>
     );
